a2ce04beef0612bae831f2a1234d1de1f9252464
pranayrauni <ipranaysingh@gmail.com>
2024-09-08 11:50:52 +0530

    JobListings, layouts, pages

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,6 +4,8 @@ const Card = ({children, bg = 'bg-gray-100'}) => {
 
     // children is used for whatever we wrap
   return (
+
+    // yaha ek card bnaya gya hai aur uske andar content rakha jayga 
     <div className={`${bg} p-6 rounded-lg shadow-md`}>{children}</div>
   )
 }
