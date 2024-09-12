953f16e6d853754b50146e37fe3964aa1a12bd08
pranayrauni <ipranaysingh@gmail.com>
2024-09-12 17:45:35 +0530

    spinner, json server proxy, job page

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,22 +1,60 @@
 import React from 'react'
-import jobs from '../jobs.json'
+// import jobs from '../jobs.json'    
+// json server laga k useState aur useEffect use kar json server chla k backend ki tarah use hoga
+import { useState, useEffect } from 'react';
 import JobListing from './JobListing'
+import Spinner from './Spinner';
 
 const JobListings = ({isHome = false}) => {
 
-    const jobListings = isHome ? jobs.slice(0, 3) : jobs;    // homepage pe 3 jiobs hi dikhana hai 
-  return (
+    // const jobListings = isHome ? jobs.slice(0, 3) : jobs;    // homepage pe 3 jiobs hi dikhana hai 
+  
+    const [jobs, setJobs] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+
+    useEffect(() => {
+      const fetchJobs = async () => {
+
+        const apiUrl = isHome ? '/api/jobs?_limit=3' : '/api/jobs'
+        // ?_limit=3 kar dene se 3 hi result m aayga agar sara dikhana ahi to normal rahnde do
+
+        try {
+          const res = await fetch(apiUrl);
+          const data = await res.json();
+          setJobs(data);
+        } catch (error) {
+          console.log('Error fetching data', error)
+        } finally {
+          setLoading(false);
+        }
+      }
+      fetchJobs();
+    }, []);
+
+  
+    return (
     <section className="bg-blue-50 px-4 py-10">
     <div className="container-xl lg:container m-auto">
       <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
         {isHome ? 'Recent Jobs' : 'All Jobs'}
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {jobListings.map((job) => (
-            <JobListing key={job.id} job={job}/>
-        ))}
+      
+        {
+          loading ? (
+            <Spinner loading={loading}/>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+
+              {jobs.map((job) => (
+                <JobListing key={job.id} job={job}/>
+              ))}
+            </div>
+          )
+        }
+        
     </div>
-    </div>
+    
   </section>
 
   )
